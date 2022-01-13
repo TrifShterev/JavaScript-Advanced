87e2e9da6ed4bfd9808c41130409e4fa37471452
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2022-01-14 00:21:16 +0200

    DynamicValidation

--- Unit Testing/Unit Testing Exercises/06.Dynamic-Validation/app.js
+++ Unit Testing/Unit Testing Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let input = document.getElementById('email');
+    let pattern = /^\w+@\w+.\w+$/gm;
+
+    input.addEventListener('change',(e)=>{
+
+        if (!pattern.test(input.value)){
+            e.target.classList.add('error')
+        }else{
+            e.target.removeAttribute('class')
+        }
+    })
 }
